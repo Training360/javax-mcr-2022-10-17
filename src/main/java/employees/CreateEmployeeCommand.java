cab23a31ad968cfd31154eb775d8bae15888c5e7
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-17 17:10:42 +0200

    Validáció

--- src/main/java/employees/CreateEmployeeCommand.java
+++ src/main/java/employees/CreateEmployeeCommand.java
@@ -1,12 +1,18 @@
 package employees;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateEmployeeCommand {
 
     @NotBlank
+    @Schema(description = "the name of the employee", example = "John Doe")
     private String name;
 }
