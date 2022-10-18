bf67fbc7e0f362f8c2debbe6bedcc05cf0dc6d53
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-18 20:19:21 +0200

    Service unit teszt

---
+++ src/test/java/employees/EmployeesServiceTest.java
@@ -0,0 +1,46 @@
+package employees;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeesServiceTest {
+
+    @Mock
+    EmployeesRepository employeesRepository;
+
+    EmployeesService employeesService;
+
+    @BeforeEach
+    void init() {
+        employeesService = new EmployeesService(employeesRepository, new EmployeesMapperImpl());
+    }
+
+    @Test
+    void testSave() {
+        employeesService.createEmployee(new CreateEmployeeCommand("John Doe"));
+
+        verify(employeesRepository).save(argThat(e -> e.getName().equals("John Doe")));
+    }
+
+    @Test
+    void testExists() {
+        when(employeesRepository.findEmployeeByNameIgnoreCase(eq("John Doe"))).thenReturn(Optional.of(new Employee(1L, "John Doe")));
+        var ex = assertThrows(EmployeeAlreadyExistsException.class, () ->
+                 employeesService.createEmployee(new CreateEmployeeCommand("John Doe")));
+        assertEquals("John Doe", ex.getName());
+    }
+
+}
