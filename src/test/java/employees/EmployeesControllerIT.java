066ddd596242f614d8d0afa677ea27b18268bb97
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-18 16:36:36 +0200

    Párhuzamos futtatás

--- src/test/java/employees/EmployeesControllerIT.java
+++ src/test/java/employees/EmployeesControllerIT.java
@@ -1,33 +1,43 @@
 package employees;
 
+import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+//import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@Sql(statements = "delete from employees")
+@Slf4j
 class EmployeesControllerIT {
 
     @Autowired
     WebTestClient webClient;
 
     @Test
+    //@RepeatedTest(2)
     void testCreateThanList() {
+        var name = "John Doe " + UUID.randomUUID().toString();
+        log.info("Name: {}", name);
         webClient.post()
                 .uri("/api/employees")
                 .contentType(MediaType.APPLICATION_JSON)
 //                .bodyValue("""
 //                        {"name": "John Doe"}
 //                        """)
-                .bodyValue(new CreateEmployeeCommand("John Doe"))
+                .bodyValue(new CreateEmployeeCommand(name))
                 .exchange()
                 .expectStatus().isCreated()
 //                .expectBody().jsonPath(".['name']", "John Doe");
-                .expectBody(EmployeeDto.class).value(e -> assertEquals("John Doe", e.getName()));
+                .expectBody(EmployeeDto.class).value(e -> assertEquals(name, e.getName()));
 
 
         webClient.get()
@@ -36,7 +46,7 @@ class EmployeesControllerIT {
                 .expectBodyList(EmployeeDto.class)
                     .value(employees -> assertThat(employees)
                             .extracting(EmployeeDto::getName)
-                            .contains("John Doe")
+                            .contains(name)
                     );
     }
 }
