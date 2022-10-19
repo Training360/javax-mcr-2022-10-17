d3e09a6e327ef024a16b8bf921fb9d50c6aa2953
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-19 11:21:06 +0200

    URL konfigból

--- employees/src/main/java/employees/addressesgateway/AddressesGateway.java
+++ employees/src/main/java/employees/addressesgateway/AddressesGateway.java
@@ -1,14 +1,20 @@
 package employees.addressesgateway;
 
 import employees.Gateway;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Gateway
+@EnableConfigurationProperties(AddressesGatewayProperties.class)
+@AllArgsConstructor
 public class AddressesGateway {
+
+    AddressesGatewayProperties addressesGatewayProperties;
 
     public Address getAddressForEmployee(String name) {
         return WebClient
-                .create("http://localhost:8081")
+                .create(addressesGatewayProperties.getUrl())
                 .get()
                 .uri(b -> b.path("/api/addresses").queryParam("name", name).build())
                 .exchangeToMono(response -> response.bodyToMono(Address.class))
